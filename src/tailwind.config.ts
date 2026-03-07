48127954e2fa16d9ef9a2efac4bd00fb0f657495
Andrey Makarychev <makarychev@reg.ru>
2026-03-07 10:50:38 +0400

    feat: initial commit

--- src/tailwind.config.ts
+++
@@ -1,6 +0,0 @@
-
-import type { Config } from 'tailwindcss';
-
-export default {
-    darkMode: 'class',
-} as Config;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'node:path';
 import vue from '@vitejs/plugin-vue';
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig(async () => ({
